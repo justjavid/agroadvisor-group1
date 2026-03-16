3d556d2c98d75ab1c1654715df2ff9c74a9b6050
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2026-03-16 15:33:50 +0400

    Calculator entities added

--- AgroBackEnd/Program.cs
+++ AgroBackEnd/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Repository.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// ... rest of your setup
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<FertilizerCalculatorDbContext>(options =>
+    options.UseNpgsql(builder.Configuration
+        .GetConnectionString("PostgreSQL")));
 
 var app = builder.Build();
 
