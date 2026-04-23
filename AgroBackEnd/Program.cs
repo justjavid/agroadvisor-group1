86e87ee0383f5b4c7e2c82c6f047d5a429947264
Ilham Abbasov <ilhamabbasov46@gmail.com>
2026-04-24 03:43:38 +0400

    Add EF migrations, Scalar UI, PostgreSQL setup

--- AgroBackEnd/Program.cs
+++ AgroBackEnd/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using Repository.Concrete;
 using Repository.Data;
 using Repository.Interfaces;
+using Scalar.AspNetCore;
 using Service;
 using System.Text;
 
@@ -12,12 +13,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Auth services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+// JWT Authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -36,6 +40,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
